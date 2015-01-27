63d08b2a31791afaa672a57464e3b74a39778b2a
Imen <imen.maazoun@gmail.com>
2015-01-27 18:48:38 +0100

    Disposable

---
+++ Gambling/Gambling.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gambling.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        bool isDisposed = false;
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+        public void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void DisposeCore()
+        {
+        }
+    }
+}
