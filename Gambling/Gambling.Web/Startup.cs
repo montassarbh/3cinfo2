11612788f3de9d9ded8b05f723fb0bd6b8a15f2e
Montassar <montassar.b.h@gmail.com>
2015-01-31 15:59:17 +0100

    31/01/2015 (2)

---
+++ Gambling/Gambling.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gambling.Web.Startup))]
+namespace Gambling.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
